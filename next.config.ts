884ec98454e2fb93f1a3c643f3989a31e4b9245f
Lee MinGi <mingi3442@gmail.com>
2025-05-26 17:59:56 +0900

    🔨 Update blue green deploy yml

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,16 @@
+import withBundleAnalyzer from '@next/bundle-analyzer';
 import type { NextConfig } from "next";
+
+const bundleAnalyzer = withBundleAnalyzer({
+  enabled: process.env.ANALYZE === 'true',
+});
 
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
+  experimental: {
+    optimizeCss: true,
+  },
 };
 
-export default nextConfig;
+export default bundleAnalyzer(nextConfig);
